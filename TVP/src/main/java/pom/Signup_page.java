c23a9d004258d1347250691a171b464e573890d0
Ashish Rai <raiashish41@gmail.com>
2019-07-21 13:11:22 +0530

    signup page elements

--- TVP/src/main/java/pom/Signup_page.java
+++ TVP/src/main/java/pom/Signup_page.java
@@ -1,5 +1,41 @@
 package pom;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class Signup_page {
+
+	@FindBy(xpath="//input[@id='fullName']")
+	WebElement fullname;
+	
+    @FindBy(xpath="//input[@id='userEmail']")
+    WebElement emailid;
+
+    @FindBy(xpath="//input[@id='companyName']")
+    WebElement companyname;
+
+    @FindBy(xpath="//input[@id='userPassword']")
+    WebElement password;
+
+    @FindBy(xpath="//input[@id='userConfirmPassword']")
+    WebElement confirmpassword;
+
+    @FindBy(xpath="//a[text()='Terms and Conditions']")
+    WebElement termsandconditionlink;
+    
+    @FindBy(xpath="//button[text()='SIGN UP']")
+    WebElement submit;
+    
+    @FindBy(xpath="//a[text()='Login']")
+    WebElement login;
+	
+	@FindBy(xpath="//a[text()='Contact Us']")
+	WebElement contactus;
+
+	@FindBy(xpath="//a[text()='Terms & Conditions']")
+	WebElement termsandcondtions;
+
+	@FindBy(xpath="//a[text()='Privacy Policy']")
+	WebElement privacypolicy;
 
 }
